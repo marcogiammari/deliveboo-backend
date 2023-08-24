a6617e141bc390adf5de2d8d19e29927e0b932b6
Emanuele Di Scala <manueldiscala@gmail.com>
2023-08-24 12:16:33 +0200

    feat(orders,ordered_product)

--- database/migrations/2023_08_24_082023_create_ordered_product_table.php
+++ database/migrations/2023_08_24_082023_create_ordered_product_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('ordered_product', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger("quantity");
             $table->timestamps();
         });
     }
