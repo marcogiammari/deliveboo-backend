5a96095059fdb9e1b51d843a019ebd190f61f417
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-08-24 16:36:57 +0200

    feat:ordered product data

---
+++ config/store/orderedProduct.php
@@ -0,0 +1,28 @@
+<?php  
+return[
+    [
+        'order_id'=>1,
+        'product_id'=>1,
+        'quantity'=>2,
+    ],
+    [
+        'order_id'=>2,
+        'product_id'=>1,
+        'quantity'=>1,
+    ],
+    [
+        'order_id'=>3,
+        'product_id'=>1,
+        'quantity'=>4,
+    ],
+    [
+        'order_id'=>4,
+        'product_id'=>1,
+        'quantity'=>2,
+    ],
+    [
+        'order_id'=>5,
+        'product_id'=>1,
+        'quantity'=>2,
+    ],
+];
