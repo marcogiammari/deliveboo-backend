d3264212963e44cd0874072e1a2a6b8fde33b994
Marco Giammari <marcogiammari@gmail.com>
2023-08-24 12:04:44 +0200

    feat: restaurant-category relationship

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Restaurant extends Model
@@ -19,5 +20,10 @@ class Restaurant extends Model
     public function products(): HasMany
     {
         return $this->hasMany(Product::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
     }
 }
