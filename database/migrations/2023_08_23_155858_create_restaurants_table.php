71ec04af3a63803fabcc0faa6ea957ea73b002fb
Emanuele Di Scala <manueldiscala@gmail.com>
2023-08-24 11:20:08 +0200

    feat(restaurant)

--- database/migrations/2023_08_23_155858_create_restaurants_table.php
+++ database/migrations/2023_08_23_155858_create_restaurants_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
+            $table->string("name", 100);
+            $table->string("vat_number", 20);
+            $table->string("note", 255)->nullable();
+            $table->foreignId("user_id")->bigint();
+            $table->string("city", 30);
+            $table->string("street_name", 50);
+            $table->string("street_number", 15);
+            $table->string("zip_code", 15);
             $table->timestamps();
         });
     }
