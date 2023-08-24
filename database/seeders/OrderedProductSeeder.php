cefe11c63a883b04ce3458a7158eb350109f92a6
Emanuele Di Scala <manueldiscala@gmail.com>
2023-08-24 19:52:20 +0200

    fix:(DBSeeder,OrderProdSeed,ProductSeeder)

--- database/seeders/OrderedProductSeeder.php
+++ database/seeders/OrderedProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class OrderedProductSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class OrderedProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $orderedproductsData = config('store.orderedProduct');
+
+        foreach ($orderedproductsData as $orderedproduct) {
+            DB::table('ordered_product')->insert([
+            'order_id' => $orderedproduct['order_id'],
+            'product_id' => $orderedproduct['product_id'],
+            'quantity' => $orderedproduct['quantity'],
+        ]);
+        }
     }
 }
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $productsData = config('store.products');
+
+        foreach ($productsData as $product) {
+            Product::create($product);
+        }
+    }
+}
