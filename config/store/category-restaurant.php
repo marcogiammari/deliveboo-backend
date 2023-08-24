6abc6752a0e9b1304f8d218c71f5f2879368439a
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-08-25 00:49:29 +0200

    create:(restaurant-category data (pivot))

---
+++ config/store/category-restaurant.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    ['restaurant_id' => 1, 'category_id' => 41],
+    ['restaurant_id' => 2, 'category_id' => 1],
+    ['restaurant_id' => 3, 'category_id' => 54],
+    ['restaurant_id' => 4, 'category_id' => 27],
+    ['restaurant_id' => 5, 'category_id' => 28],
+    ['restaurant_id' => 6, 'category_id' => 21],
+    ['restaurant_id' => 7, 'category_id' => 1],
+    ['restaurant_id' => 8, 'category_id' => 54],
+];
