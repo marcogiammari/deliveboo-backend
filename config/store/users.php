f4588d0da909f985a532dcc4db2ef11678b734bd
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-08-24 15:17:53 +0200

    feat: create users data

---
+++ config/store/users.php
@@ -0,0 +1,36 @@
+<?php 
+
+return [
+   [
+    'name'=>'ale',
+    'email'=> 'ale@mail.it',
+   ],
+   [
+    'name'=>'marco',
+    'email'=> 'marco@mail.it',
+   ],
+   [
+    'name'=>'ema',
+    'email'=> 'ema@mail.it',
+   ],
+   [
+    'name'=>'edy',
+    'email'=> 'edy@mail.it',
+   ],
+   [
+    'name'=>'giorgio',
+    'email'=> 'giorgio@mail.it',
+   ],
+   [
+    'name'=>'pippo',
+    'email'=> 'pippo@mail.it',
+   ],
+   [
+    'name'=>'gerry',
+    'email'=> 'gerry@mail.it',
+   ],
+   [
+    'name'=>'silvio',
+    'email'=> 'silio@mail.it',
+   ],
+];
