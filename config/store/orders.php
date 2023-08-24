a7a5e4f5b18303a5ab367a0f002ed66c5d902a6f
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-08-24 15:18:27 +0200

    feat: create orders data

---
+++ config/store/orders.php
@@ -0,0 +1,43 @@
+<?php 
+return [
+    [
+        'total_amount'=>10,
+        'customer_name'=>'mario',
+        'customer_address'=> 'Via dei Fazi 12',
+        'customer_tel'=>'3883856439',
+        'customer_email'=>'mariorossi@gmail.com',
+        'costumer_note'=>"chiamare all' arrivo",
+    ],
+    [
+        'total_amount'=>10,
+        'customer_name'=>'Giacomo Poretti',
+        'customer_address'=> 'Via della Catrega',
+        'customer_tel'=>'3312556357',
+        'customer_email'=>'GiacomoPoretti@gmail.com',
+        'costumer_note'=>"il citofono è rotto",
+    ],
+    [
+        'total_amount'=>10,
+        'customer_name'=>'Giovanni Storti',
+        'customer_address'=> 'Via dei Kmer 21',
+        'customer_tel'=>'3883856439',
+        'customer_email'=>'GiovanniStorti@gmail.com',
+        'costumer_note'=>"seconto piano",
+    ],
+    [
+        'total_amount'=>10,
+        'customer_name'=>'Aldo Baglio',
+        'customer_address'=> 'via capelli dalla testa',
+        'customer_tel'=>'388385763',
+        'customer_email'=>'AldoBaglio@gmail.com',
+        'costumer_note'=>"citofono ajeje brazorf",
+    ],
+    [
+        'total_amount'=>10,
+        'customer_name'=>'Maurizio Costanzo',
+        'customer_address'=> 'viale collocorto',
+        'customer_tel'=>'3314445689',
+        'customer_email'=>'MaurizioCostanzo@gmail.com',
+        'costumer_note'=>"mi affaccio alla finestra se ci arrivo",
+    ],
+];
