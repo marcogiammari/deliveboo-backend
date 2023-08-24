50c0c1189ebd4ab7a18589e6652c7b78356a940d
Marco Giammari <marcogiammari@gmail.com>
2023-08-24 11:54:11 +0200

    feat restaurant-products relationship

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Restaurant extends Model
 {
@@ -14,5 +15,9 @@ class Restaurant extends Model
     {
         return $this->belongsTo(User::class);
     }
-    
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
