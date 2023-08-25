c6843de6d37190b68ac8ba44bc35314818fbd353
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-08-25 19:22:27 +0200

    feat:(more data on products.php)

--- config/store/products.php
+++ config/store/products.php
@@ -70,5 +70,495 @@ return [
         "price" => 11.20,
         "visible" => true,
         "thumb" => "https://cdn.ilclubdellericette.it/wp-content/uploads/2019/01/linguine-allo-scoglio-1280x720.jpg"
-    ]
+    ],
+    [
+        "restaurant_id" => 1,
+        "name" => "Pasta al Pesto",
+        "price" => 8.00,
+        "description" => "Pasta con salsa al pesto",
+        "visible" => true,
+    ],
+    [
+        "restaurant_id" => 2,
+        "name" => "Insalata di Pollo",
+        "price" => 7.50,
+        "description" => "Insalata con pollo grigliato",
+        "visible" => true,
+    ],
+    [
+        "restaurant_id" => 3,
+        "name" => "Pizza Margherita",
+        "price" => 6.00,
+        "description" => "Pizza con pomodoro e mozzarella",
+        "visible" => true,
+    ],
+    [
+        "restaurant_id" => 4,
+        "name" => "Tagliolini ai Funghi",
+        "price" => 9.50,
+        "description" => "Tagliolini con funghi porcini",
+        "visible" => true,
+    ],
+    [
+        "restaurant_id" => 5,
+        "name" => "Bistecca alla Griglia",
+        "price" => 15.00,
+        "description" => "Bistecca di manzo alla griglia",
+        "visible" => true,
+    ],
+    [
+        "restaurant_id" => 6,
+        "name" => "Insalata Cesare",
+        "price" => 7.50,
+        "description" => "Insalata con pollo e salsa cesare",
+        "visible" => true,
+    ],
+    [
+        "restaurant_id" => 7,
+        "name" => "Ravioli al Tartufo",
+        "price" => 12.00,
+        "description" => "Ravioli ripieni di tartufo",
+        "visible" => true,
+    ],
+    [
+        "restaurant_id" => 8,
+        "name" => "Tiramisù",
+        "price" => 6.50,
+        "description" => "Dolce al cucchiaio con caffè e mascarpone",
+        "visible" => true,
+    ],
+    [
+        "restaurant_id" => 5,
+        "name" => "Salmone alla Griglia",
+        "price" => 13.00,
+        "description" => "Filetto di salmone alla griglia",
+        "visible" => true,
+    ],
+    [
+        "restaurant_id" => 3,
+        "name" => "Insalata Greca",
+        "price" => 6.50,
+        "description" => "Insalata con pomodori, cetrioli, olive e formaggio feta",
+        "visible" => true,
+    ],
+    [
+        "restaurant_id" => 1,
+        "name" => "Pizza Margherita",
+        "price" => 6.00,
+        "description" => "Pizza con pomodoro e mozzarella",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 6,
+        "name" => "Tagliatelle al Ragù",
+        "price" => 8.50,
+        "description" => "Tagliatelle con salsa di carne",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 1,
+        "name" => "Insalata di Riso",
+        "price" => 6.50,
+        "description" => "Insalata di riso con verdure",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" =>8,
+        "name" => "Gnocchi al Gorgonzola",
+        "price" => 9.00,
+        "description" => "Gnocchi con salsa al gorgonzola",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 1,
+        "name" => "Cotoletta alla Milanese",
+        "price" => 10.00,
+        "description" => "Cotoletta impanata fritta",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 2,
+        "name" => "Insalata di Spinaci",
+        "price" => 6.50,
+        "description" => "Insalata con spinaci freschi e frutta secca",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 3,
+        "name" => "Pasta alla Norma",
+        "price" => 7.00,
+        "description" => "Pasta con pomodoro, melanzane e ricotta salata",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 4,
+        "name" => "Pollo Arrosto",
+        "price" => 11.00,
+        "description" => "Cosce di pollo arrosto con contorno",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 5,
+        "name" => "Pizza Prosciutto e Funghi",
+        "price" => 7.50,
+        "description" => "Pizza con prosciutto cotto e funghi",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 6,
+        "name" => "Insalata di Pomodori",
+        "price" => 5.50,
+        "description" => "Insalata con pomodori freschi e basilico",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 7,
+        "name" => "Spaghetti Aglio e Olio",
+        "price" => 7.00,
+        "description" => "Spaghetti con aglio, olio e peperoncino",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 8,
+        "name" => "Insalata di Lenticchie",
+        "price" => 6.50,
+        "description" => "Insalata con lenticchie, verdure e salsa di senape",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 3,
+        "name" => "Risotto al Frutti di Mare",
+        "price" => 12.00,
+        "description" => "Risotto con frutti di mare",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 5,
+        "name" => "Pasta all'Amatriciana",
+        "price" => 8.50,
+        "description" => "Pasta con guanciale, pomodoro e pecorino",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 5,
+        "name" => "Pizza Margherita Extra",
+        "price" => 7.00,
+        "description" => "Pizza con pomodoro, mozzarella e basilico",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 2,
+        "name" => "Insalata di Quinoa",
+        "price" => 7.50,
+        "description" => "Insalata con quinoa, verdure e frutta secca",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 1,
+        "name" => "Pasta al Limone",
+        "price" => 6.00,
+        "description" => "Pasta con salsa al limone e pepe nero",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 2,
+        "name" => "Tortellini alla Panna",
+        "price" => 9.00,
+        "description" => "Tortellini con salsa alla panna",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 3,
+        "name" => "Pasta alla Puttanesca",
+        "price" => 7.50,
+        "description" => "Pasta con olive, capperi, acciughe e pomodoro",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 4,
+        "name" => "Costolette di Agnello",
+        "price" => 14.00,
+        "description" => "Costolette di agnello grigliate con contorno",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 5,
+        "name" => "Pizza Vegetariana",
+        "price" => 7.00,
+        "description" => "Pizza con verdure miste",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 6,
+        "name" => "Insalata di Avocado",
+        "price" => 6.50,
+        "description" => "Insalata con avocado, pomodori e cipolla rossa",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 7,
+        "name" => "Lasagna al Salmone",
+        "price" => 11.00,
+        "description" => "Lasagna con salmone e spinaci",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 8,
+        "name" => "Pasta alla Norma",
+        "price" => 8.50,
+        "description" => "Pasta con melanzane, pomodoro e ricotta salata",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 3,
+        "name" => "Filetto di Trota",
+        "price" => 12.00,
+        "description" => "Filetto di trota con erbe aromatiche",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 3,
+        "name" => "Pasta al Gorgonzola",
+        "price" => 7.50,
+        "description" => "Pasta con salsa al gorgonzola e noci",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 1,
+        "name" => "Pizza Prosciutto e Ananas",
+        "price" => 7.00,
+        "description" => "Pizza con prosciutto cotto e ananas",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 1,
+        "name" => "Insalata di Patate",
+        "price" => 6.50,
+        "description" => "Insalata con patate, uova e maionese",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 1,
+        "name" => "Pasta al Tartufo",
+        "price" => 9.00,
+        "description" => "Pasta con salsa al tartufo",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 2,
+        "name" => "Cappellacci di Zucca",
+        "price" => 10.50,
+        "description" => "Cappellacci ripieni di zucca con burro e salvia",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 3,
+        "name" => "Risotto alla Milanese",
+        "price" => 10.00,
+        "description" => "Risotto con zafferano e parmigiano",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 4,
+        "name" => "Pasta al Pesto di Rucola",
+        "price" => 8.50,
+        "description" => "Pasta con salsa al pesto di rucola",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 5,
+        "name" => "Insalata di Couscous",
+        "price" => 6.50,
+        "description" => "Insalata con couscous e verdure",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 6,
+        "name" => "Sformato di Verdure",
+        "price" => 7.50,
+        "description" => "Sformato di verdure con fonduta di formaggio",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 7,
+        "name" => "Pizza Capricciosa",
+        "price" => 7.00,
+        "description" => "Pizza con prosciutto, funghi, olive e carciofi",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 8,
+        "name" => "Insalata di Finocchio",
+        "price" => 6.50,
+        "description" => "Insalata con finocchio, arance e olive nere",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 8,
+        "name" => "Risotto ai Frutti di Mare",
+        "price" => 12.00,
+        "description" => "Risotto con frutti di mare misti",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 4,
+        "name" => "Pasta al Pesto di Noci",
+        "price" => 8.50,
+        "description" => "Pasta con salsa al pesto di noci",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 1,
+        "name" => "Insalata di Cetrioli",
+        "price" => 6.50,
+        "description" => "Insalata con cetrioli, yogurt e menta",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 2,
+        "name" => "Pasta alla Norma",
+        "price" => 7.50,
+        "description" => "Pasta con melanzane, pomodoro e ricotta salata",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 1,
+        "name" => "Pasta all'Arrabbiata",
+        "price" => 7.00,
+        "description" => "Pasta con salsa al pomodoro e peperoncino",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 2,
+        "name" => "Insalata di Barbabietola",
+        "price" => 6.50,
+        "description" => "Insalata con barbabietola, arance e noci",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 3,
+        "name" => "Risotto ai Funghi Porcini",
+        "price" => 12.00,
+        "description" => "Risotto con funghi porcini",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 4,
+        "name" => "Pasta al Limone",
+        "price" => 8.50,
+        "description" => "Pasta con salsa al limone e prezzemolo",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 5,
+        "name" => "Insalata di Melone",
+        "price" => 6.50,
+        "description" => "Insalata con melone, prosciutto e rucola",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 6,
+        "name" => "Gnocchi al Gorgonzola",
+        "price" => 9.00,
+        "description" => "Gnocchi con salsa al gorgonzola e noci",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 7,
+        "name" => "Pizza Tonno e Cipolla",
+        "price" => 7.00,
+        "description" => "Pizza con tonno e cipolla",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 8,
+        "name" => "Insalata di Carciofi",
+        "price" => 6.50,
+        "description" => "Insalata con cuori di carciofo, olive e pecorino",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 8,
+        "name" => "Pasta al Pesto di Pomodori Secchi",
+        "price" => 8.50,
+        "description" => "Pasta con salsa al pesto di pomodori secchi",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 5,
+        "name" => "Insalata di Peperoni",
+        "price" => 6.50,
+        "description" => "Insalata con peperoni arrostiti e olive nere",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 3,
+        "name" => "Pizza Quattro Stagioni",
+        "price" => 7.50,
+        "description" => "Pizza con quattro ingredienti diversi",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 5,
+        "name" => "Insalata di Mele",
+        "price" => 6.50,
+        "description" => "Insalata con mele, noci e formaggio",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 1,
+        "name" => "Risotto al Radicchio",
+        "price" => 8.00,
+        "description" => "Risotto con radicchio e pancetta",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 2,
+        "name" => "Pasta alle Vongole",
+        "price" => 9.50,
+        "description" => "Pasta con vongole e aglio",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 3,
+        "name" => "Insalata di Pomodori e Mozzarella",
+        "price" => 6.50,
+        "description" => "Insalata con pomodori, mozzarella e basilico",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 4,
+        "name" => "Pasta al Gorgonzola e Noci",
+        "price" => 8.50,
+        "description" => "Pasta con salsa al gorgonzola e noci",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 5,
+        "name" => "Pizza Quattro Formaggi",
+        "price" => 7.50,
+        "description" => "Pizza con quattro tipi di formaggio",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 6,
+        "name" => "Insalata di Spinaci e Pancetta",
+        "price" => 6.50,
+        "description" => "Insalata con spinaci, pancetta e uova sode",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 7,
+        "name" => "Risotto alla Zucca",
+        "price" => 9.00,
+        "description" => "Risotto con zucca e formaggio",
+        "visible" => true
+    ],
+    [
+        "restaurant_id" => 8,
+        "name" => "Pasta al Pesto di Spinaci",
+        "price" => 8.50,
+        "description" => "Pasta con salsa al pesto di spinaci",
+        "visible" => true
+    ],
 ];
