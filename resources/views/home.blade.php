4ce1d35fc7a088215f149e8e718674aa07706025
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-08-26 20:09:33 +0200

    Style:(card)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,22 @@
 @section('content')
 <div class="container-fluid mt-4">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="col-md-8 mt-5">
+            <div class="card border-white d-flex align-items-center bg-custard">
+                <img src="https://i.postimg.cc/6pb9cR1N/logo-concept-1-hd.png" alt="logo" class="w-25">
 
-                <div class="card-body">
+                <div class="card-body pb-5">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
-                    {{ __('You are logged in!') }}
+                    <h1 class="d-inline fw-bolder text-white">
+                        Che piacere rivederti 
+                    </h1>
+                    <h1 class="d-inline text-capitalize fw-bolder text-white">
+                        {{ Auth::user()->name }}
+                    </h1>
                 </div>
             </div>
         </div>
