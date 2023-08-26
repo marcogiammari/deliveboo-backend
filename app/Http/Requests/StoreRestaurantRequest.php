90a37d7edd8900f182a28f6959ffdc2b834595fb
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-08-26 11:48:53 +0200

    fix:(rules on StoreRestaurantRequest) + feat:( add rules msg error StoreRestaurantRequest)

--- app/Http/Requests/StoreRestaurantRequest.php
+++ app/Http/Requests/StoreRestaurantRequest.php
@@ -22,13 +22,31 @@ class StoreRestaurantRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "name"=>"required",
-            "vat_number"=>"required",
-            "note"=>"nullable",
-            "city" => 'required',
-            "street_name" => 'required',
-            "street_number" => 'required',
-            "zip_code" => 'required',
+            "name"=>"required|max:100",
+            "vat_number"=>"required|max:20",
+            "note"=>"nullable|max:255",
+            "city" => 'required|max:30',
+            "street_name" => 'required|max:50',
+            "street_number" => 'required|max:15',
+            "zip_code" => 'required|max:15',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'il nome è richiesto',
+            'name.max' => 'Il nome non può superare i 100 caratteri.',
+            'vat_number.required' => 'il prezzo è richiesto',
+            'note.max' => 'la descrizione non può superare i 255 caratteri',
+            'city.required' => 'La città è richiesta',
+            'city.max' => 'La città non può superare i 30 caratteri',
+            'street_name.required' => 'La via è richiesta',
+            'street_name.max' => 'Il nome della via non può superare i 50 caratteri',
+            'street_number.required' => 'Il numero civico è richiesto',
+            'street_number.max' => 'Il numero civico non può superare i 15 caratteri',
+            'zip_code.required' => 'Il codice postale è richiesto',
+            'zip_code.max' => 'Il codice postale non può superare i 15 caratteri',
         ];
     }
 }
