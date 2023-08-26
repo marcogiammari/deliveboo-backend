dbf76f7e868bc0940329f7158cd1d6e2149df4e4
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-08-26 20:10:53 +0200

    feat:(fix switch visible  bug + style)

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -2,11 +2,11 @@
 @section("content")
 <div class="container pt-5 mt-5">
 
-    <div class="card w-50 m-auto">
+    <div class="card w-50 m-auto border-white peach-bg">
         <div class="row">
             <div>
                 @if ($product->thumb)
-                <img src="{{$product->thumb}}" alt="" class="card-img-top mb-4">
+                    <img src="{{$product->thumb}}" alt="" class="card-img-top mb-4">
                 @else 
                     <img class="card-img-top" src="https://i.postimg.cc/KYST9jf9/Aggiungi.jpg" alt="">
                 @endif
@@ -14,27 +14,28 @@
         </div>
         <div class="row">
             <div>
-                <p class="fs-5 text">Nome piatto: <strong class="fs-4 text">{{$product->name}}</strong></p>
-                <p class="fs-6 text">Prezzo piatto: <strong class="fs-6 text">{{$product->price}} €</strong></p>
+                <div class="d-flex justify-content-between">
+                    <strong class="fs-4 text-white p-3">{{$product->name}}</strong>
+                    <strong class="fs-5 text-white pe-3 pt-3">{{$product->price}} € </strong>
+                </div>
                 @if ($product->description)
-                <p class="fs-6 text">Descrizione piatto: <strong class="fs-6 text text-secondary">{{$product->description}}</strong></p>
+                    <p class="ps-3 ">
+                       <strong span class="fs-5 text-white"> Descrizione:</strong> <span class="fs-6 text-white ">{{$product->description}}</span>
+                    </p>
                 @else 
-                <p class="bg-warning">Questo piatto non ha una descrizione. Vuoi inserirla?</p>
+                    <p class="bg-warning">Questo piatto non ha una descrizione. Vuoi inserirla?</p>
                 @endif
             </div>
-        </div>
-        <div class="row">
+
             <div class="justify-content-between d-flex">
                 @if ($product->visible == 1)
-                <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
-                    <label class="form-check-label" for="flexSwitchCheckChecked">Il prodotto è visibile</label>
-                </div>
+                    <div class="ps-3">
+                        <span class=" text-white fw-bold"> Disponibile: <i class="fa-solid fa-check text-success ms-1 fs-3"></i></span>
+                    </div>
                 @else 
-                <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-                    <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
-                </div>
+                    <div class="ps-3">
+                        <span class=" text-white fw-bold">Disponibile:  <i class="fa-regular fa-circle-xmark text-danger ms-1 fs-4"></i></span>
+                    </div>
                 @endif
                 <div class="m-2">
                     <form action="{{ route('products.destroy', $product) }}" method="post">
