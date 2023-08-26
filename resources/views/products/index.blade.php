b0415171e9005c749f4a2ddfef37dece856a3b6f
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-08-26 20:12:07 +0200

    feat:(fix content + style)

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,24 +3,24 @@
 @section('content')
    <div class="container">
     <div class="row">
-        <div class="col m-auto my-4">
-            <div class="card d-flex flex-row p-2 justify-content-between gap-3">
+        <div class="col-11 m-auto my-4">
+            <div class="card d-flex flex-row p-2 justify-content-between gap-3 text-dark">
                 <div class="width_product-index text-center"><span class="fw-bold fs-5">Nome</span></div>
-                <div class="width_product-index-price-table"><span class="fw-bold fs-5">Nome</span></div>
-                <div class="width_product-index text-center"><span class="fw-bold fs-5">Nome</span></div>
-                <div class="width_product-index text-center"><span class="fw-bold fs-5">Nome</span></div>
-                <div class="width_product-index text-center"><span class="fw-bold fs-5">Nome</span></div>
-                <div class="width_product-index text-center"><span class="fw-bold fs-5">Nome</span></div>
+                <div class="width_product-index-price-table"><span class="fw-bold fs-5">Prezzo</span></div>
+                <div class="width_product-index text-center"><span class="fw-bold fs-5">Descrizione</span></div>
+                <div class="width_product-index text-center"><span class="fw-bold fs-5">Immagine</span></div>
+                <div class="width_product-index text-center"><span class="fw-bold fs-5">Disponibile</span></div>
+                <div class="width_product-index text-center"><span class="fw-bold fs-5">Gestisci</span></div>
             </div>
             @foreach ($products as $product)
                 <div class="d-flex flex-row gap-3 justify-content-between p-2 border-bottom border-start border-end align-items-center">
                     <div class="width_product-index ps-3">
-                        <span>
+                        <span class=" text-black-50 fw-bold">
                             {{$product->name}}
                         </span>
                     </div>
                     <div class="width_product-index-price-table">
-                        <span>
+                        <span class=" text-black-50 fw-bold">
                             {{$product->price}} €
                         </span>
                     </div>
@@ -28,7 +28,7 @@
                     <div class="width_product-index text-center">
                         @if ($product->description)
 
-                        <span>
+                        <span class=" text-black-50 fw-bold">
                             {{$product->description}}
                         </span>
 
@@ -38,30 +38,30 @@
                     </div>
 
                     @if ($product->thumb)
-                        <div class="width_product-index text-center position-relative">
+                        <div class="width_product-index text-center position-relative image-container-custom">
                             <img src="{{$product->thumb}}" alt="" class="rounded image">
                         </div>
                     @else 
                         <div class="width_product-index text-center">
-                            <span>Questo piatto non ha ancora un'immagine</span>
+                            <span class=" text-black-50 fw-bold">Questo piatto non ha ancora un'immagine</span>
                         </div>
                     @endif
 
                     <div class="width_product-index text-center">
                         @if ($product->visible) 
-                            <span>Questo piatto è disponibile</span>
+                            <span class=" text-black-50 fw-bold"><i class="fa-solid fa-check text-success"></i></span>
                         @else 
-                            <span>Questo piatto non è attualmente disponibile ai clienti</span>
+                            <span class=" text-black-50 fw-bold"><i class="fa-regular fa-circle-xmark text-danger"></i></span>
                          @endif
                     </div>
 
                    <div class="width_product-index text-center">
-                    <a href="{{ route("products.edit", $product->id) }}" class="btn btn-primary">
-                        <span class="text-small">Modifica</span>
+                    <a href="{{ route("products.edit", $product->id) }}" class="btn btn-light bg-custard">
+                        <span class="text-small text-white">Modifica</span>
                     </a>
 
-                     <a href="{{ route("products.show", $product->id) }}" class="btn btn-success ms-2">
-                        <span class="text-small">vedi di piu</span>
+                     <a href="{{ route("products.show", $product->id) }}" class="btn btn-light peach-bg ms-2">
+                        <span class="text-small text-white">Di più</span>
                      </a>
                    </div>
                 </div>
