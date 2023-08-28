79c672133b73e9ddee8b8caa4bd6a99d47e76f96
Marco Giammari <marcogiammari@gmail.com>
2023-08-28 10:08:12 +0200

    feat: restaurant store thumb

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\StoreRestaurantRequest;
 use App\Models\Category;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class RestaurantController extends Controller
 {
@@ -28,6 +29,10 @@ class RestaurantController extends Controller
     {
         
         $data = $request->validated();
+
+        // salva nello storage l'immagine e nell'istanza il path
+        $img_path = Storage::put("uploads", $data['thumb']);
+        $data['thumb'] = $img_path;
         
         $newRestaurant = new Restaurant();
         $newRestaurant->fill($data);
--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -2,11 +2,14 @@
 @section('content')
     <div class="position-relative container">
         @if ($restaurant->thumb)
+
+            {{-- questa riga andrà cambiata prendendo l'immagine dallo storage --}}
             <img class="container-fluid" src="{{ asset($restaurant->thumb) }}" alt="{{ $restaurant->name }}">
         @else
             <img class="container-fluid" src="{{ asset('storage/placeholders/placeholder.jpg') }}"
                 alt="{{ $restaurant->name }}">
         @endif
+        {{-- da cambiare !  --}}
         <div class="card position-absolute top-100 start-50 translate-middle miaclasse" style="width: 18rem;">
             <div class="card-body">
                 <h1 class="card-title">{{ $restaurant->name }}</h1>
