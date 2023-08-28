0911c49c3a1ec65d6a13673833371a920a75510d
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-08-28 09:53:21 +0200

    feat:(style recupera password.blade)

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="container-fluid m-0 p-0 vh-100">
+    <div class="row">
+        <div class="col-5 p-0 m-0">
+            <img src="https://i.postimg.cc/j597w09D/21.jpg" alt="" class="img-fluid vh-100">
+        </div>
+        <div class="col-md-6 d-flex align-items-center justify-content-center">
+            <div class="card-custom w-75">
+                <div class="text-center fw-bolder text-white fs-2 my-4">{{ __('Recupera la tua Password') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,10 +21,10 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end text-md-end text-white fw-bolder fs-4 ">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control form-control bg-transparent-custom text-white fs-4 fw-bold @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -33,8 +36,8 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                <button type="submit" class="btn border-0 btn-light button-login-register mb-4">
+                                    <span class=" fw-bold fs-5 text-secondary text-white">{{ __('Registrati') }}</span>
                                 </button>
                             </div>
                         </div>
