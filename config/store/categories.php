76a14fceac4d97304be3a6568d9827d9faf2df7c
Emanuele Di Scala <manueldiscala@gmail.com>
2023-08-28 14:53:45 +0200

    fix(categories)

--- config/store/categories.php
+++ config/store/categories.php
@@ -5,9 +5,6 @@ return [
         "name" => "Italiano",
     ],
     [
-        "name" => "Napoletano",
-    ],
-    [
         "name" => "Cinese",
     ],
     [
@@ -17,22 +14,7 @@ return [
         "name" => "Giapponese",
     ],
     [
-        "name" => "Romano",
-    ],
-    [
-        "name" => "Sudamericano",
-    ],
-    [
-        "name" => "Coreano",
-    ],
-    [
         "name" => "Americano",
-    ],
-    [
-        "name" => "Siciliano",
-    ],
-    [
-        "name" => "Hawaiano",
     ],
     [
         "name" => "Indiano",
@@ -42,16 +24,7 @@ return [
         "name" => "Messicano",
     ],
     [
-        "name" => "Turco",
-    ],
-    [
-        "name" => "Francese",
-    ],
-    [
         "name" => "Taiwanese",
-    ],
-    [
-        "name" => "Tedesco",
     ],
     [
         "name" => "Spagnolo",
@@ -72,95 +45,11 @@ return [
         "thumb" => "https://img5.goodfon.com/original/1280x960/2/86/fastfud-burger-gamburger-pepsi-kola-kartofel-fri.jpg"
     ],
     [
-        "name" => "Dolce",
-        "thumb" => "https://images.wallpaperscraft.com/image/single/dessert_cookies_cream_199539_1280x960.jpg"
-    ],
-    [
-        "name" => "Pasta",
-    ],
-    [
-        "name" => "Gelato",
-    ],
-    [
         "name" => "Piadina",
-    ],
-    [
-        "name" => "Poke",
-        "thumb" => "https://media-cdn.tripadvisor.com/media/photo-m/1280/17/a7/1d/16/poke-bowl-ebi.jpg"
     ],
     [
         "name" => "Sushi",
         "thumb" => "https://www.arcipelagocenter.it/wp-content/uploads/2021/11/osaki_6.jpg"
-    ],
-    [
-        "name" => "Taco",
-        "thumb" => "https://www.informnny.com/wp-content/uploads/sites/58/2021/07/Taco_Dream_Job.jpg?w=1280"
-    ],
-    [
-        "name" => "Burrito",
-    ],
-    [
-        "name" => "Kebab",
-    ],
-    [
-        "name" => "Focaccia",
-        "thumb" => "https://ew3pozqeo6o.exactdn.com/wp-content/uploads/2021/04/focaccia-2.jpg?strip=all&lossy=1&webp=80&ssl=1"
-    ],
-    [
-        "name" => "Frullato",
-        "thumb" => "https://static.toiimg.com/thumb/msid-82736662,width-1280,resizemode-4/82736662.jpg"
-    ],
-    [
-        "name" => "Centrifuga",
-    ],
-    [
-        "name" => "Bubble tea",
-        "thumb" => "https://cdn.foodaciously.com/static/recipes/9d082354-2258-46b7-8e5c-c9bf25dfcc25/lychee-bubble-tea-1ea47f1059d3b7645c0b2027979ccc58-1280-q80.jpg"
-    ],
-    [
-        "name" => "Insalata",
-        "thumb" => "https://media.self.com/photos/5a382348e1ef6d3e6b88032f/master/pass/Salad.jpg"
-    ],
-    [
-        "name" => "Pinsa",
-    ],
-    [
-        "name" => "Ramen",
-    ],
-    [
-        "name" => "Tapas",
-    ],
-    [
-        "name" => "Panuozzo",
-    ],
-    [
-        "name" => "Toast",
-        "thumb" => "https://media.self.com/photos/5a04880efe72996c37edd020/master/pass/brussels-sprout-pear-grilled-cheese.jpg"
-    ],
-    [
-        "name" => "Noodle",
-        "thumb" => "https://static.toiimg.com/thumb/msid-69130460,width-1280,resizemode-4/69130460.jpg"
-    ],
-    [
-        "name" => "Panino",
-        "thumb" => "https://www.yummology.com/upload/image/qi/2v/qi2vb5u0fv-4x3-xl.jpg"
-    ],
-    [
-        "name" => "Pesce",
-    ],
-    [
-        "name" => "Carne",
-    ],
-    [
-        "name" => "Frutta",
-        "thumb" => "https://wallpapersmug.com/download/1280x960/51fc2e/fruits-fresh.jpg"
-    ],
-    [
-        "name" => "Verdura",
-    ],
-    [
-        "name" => "Fritto",
-        "thumb" => "https://media.houseandgarden.co.uk/photos/6189508cf9cf3d171adad8b5/4:3/w_1280,h_960,c_limit/housegarden_americancomfort_feb_5538-house-6feb18-chris-tonnesen.jpg"
     ],
     [
         "name" => "Vegano",
@@ -169,13 +58,10 @@ return [
         "name" => "Vegetariano",
     ],
     [
-        "name" => "Senza glutine",
+        "name" => "Gluten Free",
     ],
     [
-        "name" => "Healty",
-    ],
-    [
-        "name" => "Gastronomia",
+        "name" => "Healthy",
     ],
     [
         "name" => "Fast food"
