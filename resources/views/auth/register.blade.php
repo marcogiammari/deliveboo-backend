aa72fe1fff2c835020feeff17c99bc5047e66f72
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-08-28 14:29:13 +0200

    fix(tradotto cognome)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,7 +31,7 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="surname" class="col-md-4 col-form-label text-md-end text-white fw-bolder fs-4">Surname</label>
+                            <label for="surname" class="col-md-4 col-form-label text-md-end text-white fw-bolder fs-4">Cognome</label>
                             <div class="col-md-6">
                                 <input id="surname" type="text" class="form-control bg-transparent-custom text-white fs-4 fw-bold @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
 
