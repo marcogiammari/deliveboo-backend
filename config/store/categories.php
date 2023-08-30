f567a27a39c45462a6e58b883d2e42f2787b92ed
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-08-30 18:21:13 +0200

    feat menu restaurant

--- config/store/categories.php
+++ config/store/categories.php
@@ -3,47 +3,62 @@
 return [
     [   
         "name" => "Americano",
+        // 1
     ],
     [
         "name" => "Cinese",
+        // 2
     ],
     [
         "name" => "Fast Food",
+        // 3
     ],
     [
         "name" => "Giappopnese",
+        // 4
     ],
     [
         "name" => "Greco",
+        //5
     ],
     [
         "name" => "Hamburger",
+        //6
     ],
     [
         "name" => "Helthy",
+        //7
     ],
     [
         "name" => "Indiano",
+        //8
     ],
     [
         "name" => "Italiana",
+        //9
     ],
     [
         "name" => "Messicano",
+        //10
     ],
     [
         "name" => "Pesce",
+        //11
     ],
     [
         "name" => "Pizza",
+        //12
     ],
     [
         "name" => "Poke",
+        //13
     ],
     [
         "name" => "Senza Glutine",
+        //14
     ],
     [
         "name" => "Vegano",
+        //15
     ]
 ];
