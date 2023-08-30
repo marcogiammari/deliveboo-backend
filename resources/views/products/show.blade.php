024b233507b2294cfb7743a55face7be52802d42
Emanuele Di Scala <manueldiscala@gmail.com>
2023-08-30 11:06:03 +0200

    feat(modal removing product)

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -33,22 +33,22 @@
                     <form action="{{ route('products.destroy', $product) }}" method="post">
                         @csrf
                         @method('DELETE')
-                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#confirmDeleteModal">
-                            Cancella il prodotto
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Launch demo modal
                         </button>
-                        <div class="modal" tabindex="0" id="confirmDeleteModal">
+                        
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title">Modal title</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <p>Modal body text goes here.</p>
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Sei sicuro di voler eliminare {{$product->name}}</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
                                     <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Save changes</button>
                                     </div>
                                 </div>
                             </div>
