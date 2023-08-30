d409e06d4d0ed0d73396617e3a9244ba7b8f6a09
Edy Galdiolo <edygaldy302@gmail.com>
2023-08-30 18:58:46 +0200

    Fix: content with new settings

--- resources/views/restaurants/create.blade.php
+++ resources/views/restaurants/create.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-8 offset-2">
-                <h1>Crea il tuo ristorante</h1>
-                <form id="createRestaurant" action="{{ route('restaurants.store') }}" class="needs-validation" method="post"
+    <div>
+        <div>
+            <div>
+                <h1 class="text-center mt-5">Crea il tuo ristorante</h1>
+                <form id="createRestaurant" action="{{ route('restaurants.store') }}" class="d-flex flex-column gap-3 align-items-center needs-validation" method="post"
                     enctype="multipart/form-data">
                     @csrf
-                    <div class="mb-3">
+                    <div class="mb-3 w-75">
                         <label for="name" class="form-label">Nome del ristorante <span class="{{ $errors->has('name') ? 'text-danger' : '' }}">*</span></label>
                         <input type="text" name="name" id="name"  value="{{old('name')}}" required
                             class="form-control @error('name') is-invalid @enderror">
@@ -17,7 +17,7 @@
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 w-75">
                         <label for="vat_number" class="form-label">Partita Iva <span class="{{ $errors->has('vat_number') ? 'text-danger' : '' }}">*</span></label>
                         <input type="text" name="vat_number" id="vat_number" value="{{old('vat_number')}}"
                             class="form-control @error('vat_number') is-invalid @enderror" required>
@@ -25,14 +25,14 @@
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 w-75">
                         <label for="note" class="form-label">Vuoi aggiungere una descrizione?</label>
                         <textarea name="note" id="note" class="form-control @error('note') is-invalid @enderror" rows="4"></textarea>
                         @error('note')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 w-75">
                         <label for="thumb" class="form-label">Inserisci un'immagine</label>
                         <input type="file" name="thumb" id="thumb"
                             class="form-control @error('thumb') is-invalid @enderror">
@@ -41,7 +41,7 @@
                         @enderror
                     </div>
 
-                    <div class="mb-3">
+                    <div class="mb-3 w-75">
                         <label for="street_name" class="form-label">Via <span class="{{ $errors->has('street_name') ? 'text-danger' : '' }}">*</span></label>
                         <input type="text" name="street_name" id="street_name" required value="{{old('street_name')}}"
                             class="form-control @error('street_name') is-invalid @enderror">
@@ -49,7 +49,7 @@
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 w-75">
                         <label for="street_number" class="form-label">N° <span class="{{ $errors->has('street_number') ? 'text-danger' : '' }}">*</span></label>
                         <input type="text" name="street_number" id="street_number" required value="{{old('street_number')}}"
                             class="form-control @error('street_number') is-invalid @enderror">
@@ -57,7 +57,7 @@
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 w-75">
                         <label for="zip_code" class="form-label">CAP <span class="{{ $errors->has('zip_code') ? 'text-danger' : '' }}">*</span></label>
                         <input type="text" name="zip_code" id="zip_code" required value="{{old('zip_code')}}"
                             class="form-control @error('zip_code') is-invalid @enderror">
