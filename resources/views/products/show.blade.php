f367e247f4b6448c073bf72c6a92f9e20c6a478d
Emanuele Di Scala <manueldiscala@gmail.com>
2023-08-30 11:08:59 +0200

    fix(modal)

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -43,12 +43,12 @@
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Sei sicuro di voler eliminare {{$product->name}}</h1>
+                                    <h1 class="modal-title fs-5 m-auto" id="exampleModalLabel">Sei sicuro di voler eliminare {{$product->name}}</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
-                                    <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="submit" class="btn btn-primary">Save changes</button>
+                                    <div class="modal-footer d-flex justify-content-center">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Torna indietro</button>
+                                    <button type="submit" class="btn btn-danger">Rimuovi</button>
                                     </div>
                                 </div>
                             </div>
