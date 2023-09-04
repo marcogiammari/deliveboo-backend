2293dddc86592aaa0f2e0146a3be3f720e75c55f
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-09-04 17:06:53 +0200

    ffeat:8cards on /home only style)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,31 +3,27 @@
 @section('content')
 <div class="container-fluid mt-4">
     <div class="row justify-content-center">
-        <div class="col-md-8 mt-5">
-            <div class="card border-white d-flex align-items-center bg-custard">
-                <img src="https://i.postimg.cc/6pb9cR1N/logo-concept-1-hd.png" alt="logo" class="w-25">
-
-                {{-- <div class="card-body pb-5">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-                    <h1 class="d-inline fw-bolder text-white">
-                        Che piacere rivederti 
-                    </h1>
-                    <h1 class="d-inline text-capitalize fw-bolder text-white">
-                        {{ Auth::user()->name }}
-                        {{ Auth::user()->surname }} 
-                    </h1>
-                </div> --}}
+        <div class="mt-5 d-flex justify-content-between">
+            <div class="border-white d-flex justify-content-center align-items-center custom-feedback-card card-1 ">
+                <h3 class="text-capitalize fw-bolder text-white">
+                    12,00 €
+                </h3>
+            </div>
+            <div class="border-white d-flex justify-content-center align-items-center custom-feedback-card card-2 ">
+                <h3 class="text-capitalize fw-bolder text-white">
+                    Qui un mini chart
+                </h3>
+            </div>
+            <div class="border-white d-flex justify-content-center align-items-center custom-feedback-card card-3 ">
+                <h3 class="text-capitalize fw-bolder text-white">
+                    Qui una lista di prodotti
+                </h3>
             </div>
         </div>
     </div>
-    {{$orders}}
-    <div class=" m-auto mt-5">
+    <div class=" m-auto mt-5 border border-black-50 rounded py-3">
         <h1 class="text-center">I tuoi Ordini</h1>
-        <div class="d-flex justify-content-between">
+        <div class="d-flex justify-content-between border-bottom border-black-50 fw-semibold">
             <span class="fs-5 custom-table">Nome</span>
             <span class="fs-5 custom-table">E-mail</span>
             <span class="fs-5 custom-table">Tel.</span>
@@ -36,7 +32,7 @@
             <span class="fs-5 custom-table">Totale</span>
         </div>
         @foreach ($orders as $order)
-        <div class="d-flex justify-content-between">
+        <div class="d-flex justify-content-between mt-2 border-bottom border-black-50 py-2">
             <span class="fs-5 custom-table">{{$order->customer_name}}</span>
             <span class="fs-5 custom-table">{{$order->customer_email}}</span>
             <span class="fs-5 custom-table">{{$order->customer_tel}}</span>
