18ba25c3b7645bd267dfc5b14b9787fd1d914bf3
Marco Giammari <marcogiammari@gmail.com>
2023-09-08 12:44:11 +0200

    fix: month_income / best_selling_prod whereHas(user_id)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,16 +32,42 @@ class HomeController extends Controller
             $query->where('users.id', $user_id);
         })->get();
 
-        $month_income = Order::where('is_paid', true)->whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->sum('total_amount');
+        $month_income = Order::where('is_paid', true)->whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->whereHas('products.restaurant.user', function ($query) use ($user_id) {
+            $query->where('users.id', $user_id);
+        })->sum('total_amount');
 
         $best_selling_product = Product::select('products.name')
             ->join('order_product', 'products.id', '=', 'order_product.product_id')
             ->join('orders', 'order_product.order_id', '=', 'orders.id')
             ->where('is_paid', true)
+            ->whereHas('restaurant.user', function ($query) use ($user_id) {
+                $query->where('users.id', $user_id);
+            })
             ->whereMonth('orders.created_at', Carbon::now()->month)
             ->groupBy('products.name')
             ->orderByRaw('SUM(order_product.quantity) DESC')
             ->first();
+
+        
+        // $months = null;
+        // $incomes = null;
+
+        // $data = [
+        //     'mese' => 'income',
+        //     'mese' => 'income',
+        //     'mese' => 'income',
+        //     'mese' => 'income',
+        // ];
+
+            // const data = [
+            //     { year: 2010, count: 10 },
+            //     { year: 2011, count: 20 },
+            //     { year: 2012, count: 15 },
+            //     { year: 2013, count: 25 },
+            //     { year: 2014, count: 22 },
+            //     { year: 2015, count: 30 },
+            //     { year: 2016, count: 28 },
+            //   ];
         
         return view('home', compact('orders', 'month_income', 'best_selling_product'));
     }
