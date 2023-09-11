cf4b60533de68c1d2135c36a25c9dbe7714742e3
Marco Giammari <marcogiammari@gmail.com>
2023-09-11 13:24:23 +0200

    feat: StatsController queries WIP

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,22 +32,6 @@ class HomeController extends Controller
             $query->where('users.id', $user_id);
         })->paginate(15);
 
-        $month_income = Order::where('is_paid', true)->whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->whereHas('products.restaurant.user', function ($query) use ($user_id) {
-            $query->where('users.id', $user_id);
-        })->sum('total_amount');
-
-        $best_selling_product = Product::select('products.name')
-            ->join('order_product', 'products.id', '=', 'order_product.product_id')
-            ->join('orders', 'order_product.order_id', '=', 'orders.id')
-            ->where('is_paid', true)
-            ->whereHas('restaurant.user', function ($query) use ($user_id) {
-                $query->where('users.id', $user_id);
-            })
-            ->whereMonth('orders.created_at', Carbon::now()->month)
-            ->groupBy('products.name')
-            ->orderByRaw('SUM(order_product.quantity) DESC')
-            ->first();
-
         // daily income chart data 
         $day_income = Order::selectRaw('DAY(created_at) as day, SUM(total_amount) as incomes')
             ->where('is_paid', true)
@@ -67,7 +51,7 @@ class HomeController extends Controller
             ];
         });
         // Raggruppa gli ordini per anno e calcola il conteggio di ciascun gruppo
-        $ordersData = Order::selectRaw('MONTH(created_at) as month, SUM(total_amount) as incomes')
+        $month_income = Order::selectRaw('MONTH(created_at) as month, SUM(total_amount) as incomes')
             ->where('is_paid', true)
             ->whereYear('created_at', Carbon::now()->year)
             ->whereHas('products.restaurant.user', function ($query) use ($user_id) {
@@ -77,7 +61,7 @@ class HomeController extends Controller
             ->get();
 
         // Trasforma i risultati in un array nel formato desiderato
-        $monthly_data = $ordersData->map(function ($item) {
+        $monthly_data = $month_income->map(function ($item) {
             return [
                 'month' => $item->month,
                 'incomes' => $item->incomes,
--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -1,13 +1,58 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+
 
 class StatsController extends Controller
 {
-    public function index() 
+    public function index()
     {
 
-        return view('stats');
+        $user_id = Auth::user()->id;
+
+        // incomes 
+        $day_income = Order::where('is_paid', true)->whereDate('created_at', Carbon::now()->format('Y-m-d'))->whereHas('products.restaurant.user', function ($query) use ($user_id) {
+            $query->where('users.id', $user_id);
+        })->sum('total_amount');
+
+        $month_income = Order::where('is_paid', true)->whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->whereHas('products.restaurant.user', function ($query) use ($user_id) {
+            $query->where('users.id', $user_id);
+        })->sum('total_amount');
+
+        $year_income = Order::where('is_paid', true)->whereYear('created_at', Carbon::now()->year)->whereHas('products.restaurant.user', function ($query) use ($user_id) {
+            $query->where('users.id', $user_id);
+        })->sum('total_amount');
+
+        $total_income = Order::where('is_paid', true)->whereHas('products.restaurant.user', function ($query) use ($user_id) {
+            $query->where('users.id', $user_id);
+        })->sum('total_amount');
+
+        // ----------------- //
+        $best_selling_product = Product::select('products.name')
+        ->join('order_product', 'products.id', '=', 'order_product.product_id')
+        ->join('orders', 'order_product.order_id', '=', 'orders.id')
+        ->where('is_paid', true)
+        ->whereHas('restaurant.user', function ($query) use ($user_id) {
+            $query->where('users.id', $user_id);
+        })
+        ->whereMonth('orders.created_at', Carbon::now()->month)
+        ->groupBy('products.name')
+        ->orderByRaw('SUM(order_product.quantity) DESC')
+        ->first();
+
+        var_dump($day_income);
+        var_dump($month_income);
+        var_dump($year_income);
+        var_dump($total_income);
+        var_dump($best_selling_product);
+        // var_dump($worst_selling_product);
+
+        return view('stats', compact('day_income','month_income', 'year_income', 'total_income', 'best_selling_product'));
     }
 }
