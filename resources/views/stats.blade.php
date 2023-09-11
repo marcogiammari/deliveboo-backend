f05e0ed37bd859905742970a176fc24d47fa3430
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-09-11 13:25:30 +0200

    feat:(initial structure of page stats)

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -4,4 +4,36 @@
 @section('content')
 
 
+<div class="w-100 d-flex flex-wrap justify-content-evenly  py-2">
+    <div class="stats-cards rounded my-3">
+
+    </div>
+    <div class="stats-cards rounded my-3">
+
+    </div>
+    <div class="stats-cards rounded my-3">
+
+    </div>
+    <div class="stats-cards rounded my-3">
+
+    </div>
+
+
+    <div class="d-flex justify-content-between">
+        <div class="w-50 p-3">
+
+        </div>
+
+        <div class="w-50 p-3">
+
+        </div>
+    </div>
+
+
+    <div class="d-flex">
+        <div></div>
+        <div></div>
+    </div>
+</div>
+
 @endsection
